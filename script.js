e60db39ddfaddd806d8e40e818ebe528543b54a5
Erikkk999 <193602698+Erikkk999@users.noreply.github.com>
2025-12-15 16:34:55 +0100

    feat: implement function to play one round
    also includes adding the score variables and minor style fixes

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
     switch (choice) {
         case 0:
-        return "Rock"
+        return "rock"
         case 1:
-        return "Paper"
+        return "paper"
         default:
-        return"Scissors"
+        return"scissors"
     }
 }
 
@@ -15,4 +15,16 @@ function getHumanChoice() {
     return input.toLowerCase();
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Draw!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
