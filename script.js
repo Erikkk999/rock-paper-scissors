a40886c12af82b05e0babbb13e9bc6a8c8f42673
Erikkk999 <193602698+Erikkk999@users.noreply.github.com>
2025-12-15 22:14:12 +0100

    feat: add loop to play the full game
    also refactor some of the code and add a new function

--- script.js
+++ script.js
@@ -15,33 +15,43 @@ function getHumanChoice() {
     return input.toLowerCase();
 }
 
+function playGame() {
+
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
+    function playRound(humanChoice, computerChoice) {
+        
+        console.log(`Round ${i + 1}\nPlayer: ${humanChoice} | Computer: ${computerChoice}`);
+        
+        if (humanChoice === computerChoice) {
+            console.log("Draw!");
 
-    if (humanChoice === computerChoice) {
-        console.log("Draw!");
+        } else if ((humanChoice === "rock" && computerChoice === "paper")
+            || (humanChoice === "paper" && computerChoice === "scissors")
+            || (humanChoice === "scissors" && computerChoice === "rock")) {
+            console.log(`${computerChoice} beats ${humanChoice} | Computer Wins!`);
+            ++computerScore;
 
-    } else if (humanChoice === "rock" && computerChoice === "paper"
-        || humanChoice === "paper" && computerChoice === "scissors"
-        || humanChoice === "scissors" && computerChoice === "rock") {
-        console.log(`${computerChoice} beats ${humanChoice} | Computer Wins!`);
-         ++computerScore;
+        } else if ((computerChoice === "rock" && humanChoice === "paper")
+            || (computerChoice === "paper" && humanChoice === "scissors")
+            || (computerChoice === "scissors" && humanChoice === "rock")) {
+            console.log(`${humanChoice} beats ${computerChoice} | Player Wins!`);
+            ++humanScore;
 
-    } else if (computerChoice === "rock" && humanChoice === "paper"
-        || computerChoice === "paper" && humanChoice === "scissors"
-        || computerChoice === "scissors" && humanChoice === "rock") {
-        console.log(`${humanChoice} beats ${computerChoice} | Player Wins!`);
-        ++humanScore;
-
-    } else {
-        alert("Invalid choice!");
+        } else {
+            alert("Invalid choice!");
+        }
+        console.log(`Score\nPlayer: ${humanScore} | Computer: ${computerScore}`);
     }
 
+    let i = 0;
+    for (;i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+
+    }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
